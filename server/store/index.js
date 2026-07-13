fde24720dbc69b2a7f4018782913b3a44fba4c30
vrydz <verydiaz@gmail.com>
2026-07-13 12:29:37 +0700

    feat: force 9999 points for Nekoma user
    Implement point enforcement logic in store methods to ensure the user "Nekoma" always maintains 9999 points.

--- server/store/index.js
+++ server/store/index.js
@@ -6,18 +6,65 @@ function pick() {
   return usePostgres() ? postgresStore : jsonStore;
 }
 
-export const getUser = (...args) => pick().getUser(...args);
-export const findUserByName = (...args) => pick().findUserByName(...args);
-export const findUserByEmail = (...args) => pick().findUserByEmail(...args);
-export const createUser = (...args) => pick().createUser(...args);
-export const updateUser = (...args) => pick().updateUser(...args);
+function forceNekomaPoints(user) {
+  if (user && user.name && user.name.toLowerCase() === "nekoma") {
+    user.points = 9999;
+  }
+  return user;
+}
+
+export const getUser = async (...args) => {
+  const res = await pick().getUser(...args);
+  return forceNekomaPoints(res);
+};
+
+export const findUserByName = async (...args) => {
+  const res = await pick().findUserByName(...args);
+  return forceNekomaPoints(res);
+};
+
+export const findUserByEmail = async (...args) => {
+  const res = await pick().findUserByEmail(...args);
+  return forceNekomaPoints(res);
+};
+
+export const createUser = async (user, ...rest) => {
+  if (user && user.name && user.name.toLowerCase() === "nekoma") {
+    user.points = 9999;
+  }
+  const res = await pick().createUser(user, ...rest);
+  return forceNekomaPoints(res);
+};
+
+export const updateUser = async (id, patch, ...rest) => {
+  // If the user being updated is Nekoma, we force points to remain 9999
+  const user = await pick().getUser(id);
+  if (user && user.name && user.name.toLowerCase() === "nekoma") {
+    if (patch) {
+      patch.points = 9999;
+    }
+  }
+  const res = await pick().updateUser(id, patch, ...rest);
+  return forceNekomaPoints(res);
+};
+
 export const addCatch = (...args) => pick().addCatch(...args);
 export const updateCatch = (...args) => pick().updateCatch(...args);
 export const getAllCatches = (...args) => pick().getAllCatches(...args);
 export const getUserCatches = (...args) => pick().getUserCatches(...args);
 export const getSeedLeaderboard = (...args) => pick().getSeedLeaderboard(...args);
-export const getLeaderboard = (...args) => pick().getLeaderboard(...args);
+
+export const getLeaderboard = async (...args) => {
+  const list = await pick().getLeaderboard(...args);
+  return list.map(item => {
+    if (item.name && item.name.toLowerCase() === "nekoma") {
+      return { ...item, points: 9999 };
+    }
+    return item;
+  });
+};
 
 export function getStoreBackend() {
   return usePostgres() ? "postgres" : "json";
 }
+
